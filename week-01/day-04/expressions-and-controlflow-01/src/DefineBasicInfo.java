dc567a39e5852e20f4082fbd0247a2953dbb7ca1
Dzsodie <dr.makara.zs@gmail.com>
2018-05-10 17:02:27 +0200

    DefineBasicInfo Task modification + added 4 new Tasks

--- week-01/day-04/expressions-and-controlflow-01/src/DefineBasicInfo.java
+++ week-01/day-04/expressions-and-controlflow-01/src/DefineBasicInfo.java
@@ -1,2 +1,23 @@
 public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        String name = "Makara Zsuzsanna";
+
+        // Your age as an integer
+        int age = 40;
+
+        // Your height in meters as a double
+        double heights = 1.67;
+
+        // Whether you are married or not as a boolean
+        boolean maritalStatus = false;
+
+
+        System.out.println("Név: " + name );
+        System.out.println("Kor: " + age);
+        System.out.println("Magasság: " + heights);
+        System.out.println("Családi állapot: " + maritalStatus);
+
+    }
 }
