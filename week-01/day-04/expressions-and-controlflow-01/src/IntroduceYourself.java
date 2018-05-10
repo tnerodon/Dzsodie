a6d6e669fb8d726cf1e9f7d2af41307bca8a8f1c
Dzsodie <dr.makara.zs@gmail.com>
2018-05-10 16:19:19 +0200

    IntroduceYourself Task

---
+++ week-01/day-04/expressions-and-controlflow-01/src/IntroduceYourself.java
@@ -0,0 +1,24 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        // Write a program that prints a few details to the terminal window about you
+        // It should print each detail to a new line.
+        //  - Your name
+        //  - Your age
+        //  - Your height in meters (as a decimal fraction)
+        //
+        //  Example output:
+        //  John Doe
+        //  31
+        //  1.87
+        //name is given
+        String name = "Makara Zsuzsanna";
+        //age is set
+        int age = 40;
+        //height is given in meters
+        double height = 1.67;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+}
