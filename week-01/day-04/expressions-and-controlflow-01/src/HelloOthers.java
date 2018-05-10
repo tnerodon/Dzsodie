36f523ff17b7e6756f9695547a6c7e9bd35c0010
Dzsodie <dr.makara.zs@gmail.com>
2018-05-10 16:16:14 +0200

    HelloOthers task

--- week-01/day-04/expressions-and-controlflow-01/src/HelloOthers.java
+++ week-01/day-04/expressions-and-controlflow-01/src/HelloOthers.java
@@ -1,2 +1,15 @@
 public class HelloOthers {
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+
+        // Program greets three people
+        System.out.println("Hello, Franci!");
+        System.out.println("Hello, Döme!");
+        System.out.println("Hello, Krisz!");
+    }
 }
