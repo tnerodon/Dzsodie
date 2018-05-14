60bf437381ab406bd9ac37bfe128f661a466d4f5
Dzsodie <dr.makara.zs@gmail.com>
2018-05-14 09:01:10 +0200

    Diamond and Pyramid exercises

--- week-01/day-04/expressions-and-controlflow-01/src/DrawDiamond.java
+++ week-01/day-04/expressions-and-controlflow-01/src/DrawDiamond.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class DrawDiamond {
     // Write a program that reads a number from the standard input, then draws a
     // diamond like this:
@@ -13,6 +15,25 @@ public class DrawDiamond {
     //
     // The diamond should have as many lines as the number was
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, give me a number!");
+        int level = scanner.nextInt();
+        for (int i = 0; i < level; i++){
+            for (int j = 0; j < level-i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+            for (int j = 0; j > level+i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k >= i; k--) {
+                System.out.print("* ");
+            }
+        }
+            System.out.println(" ");
+        }
 
     }
-}
--- week-01/day-04/expressions-and-controlflow-01/src/DrawPyramid.java
+++ week-01/day-04/expressions-and-controlflow-01/src/DrawPyramid.java
@@ -13,33 +13,21 @@ public class DrawPyramid {
     // The pyramid should have as many lines as the number was
 
     public static void main(String[] args) {
-        //Scanner scanner = new Scanner(System.in);
-        // System.out.println("Please, give me the number of the levels of the pyramid!");
-        // int level = scanner.nextInt();
-
-        //for (int i = 1; i <= level; i++){
-        // for (int j =1; j <= (level + (level - 1)); j++){
-        //    if(((level - j) < i) && (i < (level + j)))  {
-        //       System.out.println("*");
-        //   } else System.out.println(" ");
-        //}
-        //}
-        // System.out.println(" ");
-
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, give me the number of the levels of the pyramid!");
+        int level = scanner.nextInt();
 
-        System.out.println("How tall pyramid do you wish for, oh pharaoh?");
-        int triangleRows = scanner.nextInt();
-
-        for (int row = 0; row < triangleRows + 1; row++) {
-            for (int spaces = triangleRows; spaces > row; spaces--) {
+        for (int i = 0; i < level; i++) {
+            for (int j = 0; j < level-i; j++) {
                 System.out.print(" ");
             }
-            
-            for (int col = 0; col < ((row * 2) - 1); col++) {
-                System.out.print("*");
+            for (int k = 0; k <= i; k++) {
+                System.out.print("* ");
             }
+            System.out.println();
 
         }
+
     }
 }
+
