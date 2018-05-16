6c9a03387533ab8c2a1988cdeb189422471c921a
Dzsodie <dr.makara.zs@gmail.com>
2018-05-16 14:32:36 +0200

    multiple lines

--- week-02/day-03/FileManipulation-Exeptions-ErrorHandling/src/TicTacToe.java
+++ week-02/day-03/FileManipulation-Exeptions-ErrorHandling/src/TicTacToe.java
@@ -1,2 +1,19 @@
 public class TicTacToe {
+  // Write a function that takes a filename as string,
+  // open and read it. The file data represents a tic-tac-toe
+  // game result. Return 'X'/'O'/'draw' based on which player
+  // has winning situation.
+  public static void main(String[] args) {
+
+
+
+       // System.out.println(ticTacResult("win-o.txt"))
+          // should print O
+
+         // System.out.println(ticTacResult("win-x.txt"))
+          // should print X
+
+         // System.out.println(ticTacResult("draw.txt"))
+          // should print draw
+  }
 }
--- week-02/day-03/FileManipulation-Exeptions-ErrorHandling/src/WriteSingleLine.java
+++ week-02/day-03/FileManipulation-Exeptions-ErrorHandling/src/WriteSingleLine.java
@@ -1,2 +1,24 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class WriteSingleLine {
+  public static void main(String[] args) {
+    // Open a file called "my-file.txt"
+    // Write your name in it as a single line
+    // If the program is unable to write the file,
+    // then it should print an error message like: "Unable to write file: my-file.txt"
+    List<String> fileContent = new ArrayList();
+    fileContent.add("Dzsodie");
+    try {
+      Path filePath = Paths.get("my-file.txt");
+      Files.write(filePath, fileContent);
+    }
+    catch (IOException e){
+      System.out.println("Unable to write file: my-file.txt");
+    }
+  }
 }
